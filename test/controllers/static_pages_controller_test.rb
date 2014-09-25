66442ee77a9e3dcf2d9a06a5e03b9e3b75525c56
lrusli <rusli.lawrence@gmail.com>
2014-09-25 15:34:58 -0700

    Finish chapter 3 exercises

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,32 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "RoR Tutorial"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "home | RoR Tutorial"
+    assert_select "title", "home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "help | RoR Tutorial"
+    assert_select "title", "help | #{@base_title}"
   end
 
   test "should get about" do
   	get :about
   	assert_response :success
-  	assert_select "title", "about | RoR Tutorial"
+  	assert_select "title", "about | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "contact | #{@base_title}"
   end
 end
